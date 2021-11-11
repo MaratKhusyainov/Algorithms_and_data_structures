6dccb2dcf6b2f2cc6ca19e18145727e13b9edd68
MaratKhusyainov <khusyainov.mar@yandex.ru>
2021-11-11 17:17:59 +0300

    Выполнено домашнее задание № 5

---
+++ src/com/geekbrains/hometask5/Thing.java
@@ -0,0 +1,20 @@
+package com.geekbrains.hometask5;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Thing  {
+
+    private String name;
+    private int price;
+    private int weight;
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
